da2f331b27f5005c05d17f540d49b5f54bd494f4
André Neidert <acneidert@gmail.com>
2017-12-04 19:00:30 -0200

    Adicionando Classe de Perfil

---
+++ app/src/main/java/com/puzzle/rpgvida/Perfil.java
@@ -0,0 +1,82 @@
+package com.puzzle.rpgvida;
+
+/**
+ * Created by acneidert on 04/12/17.
+ */
+
+public class Perfil {
+    private long id;
+    private String nome;
+    private String user;
+    private String senha;
+    private int xp;
+    private boolean isLogged;
+
+    public Perfil() {
+    }
+
+    public boolean isLogged() {
+        return isLogged;
+    }
+
+    public void setLogged(boolean logged) {
+        isLogged = logged;
+    }
+
+    public Perfil(long id) {
+        this.id = id;
+    }
+
+    public Perfil(String user, String senha) {
+        this.user = user;
+        this.senha = senha;
+    }
+
+    public Perfil(long id, String nome, String user, String senha, int xp) {
+        this.id = id;
+        this.nome = nome;
+        this.user = user;
+        this.senha = senha;
+        this.xp = xp;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+}
---
+++ app/src/main/java/com/puzzle/rpgvida/db/PerfilDB.java
@@ -0,0 +1,152 @@
+package com.puzzle.rpgvida.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.puzzle.rpgvida.Perfil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by acneidert on 04/12/17.
+ */
+
+public class PerfilDB extends SQLiteOpenHelper {
+    public static final String NOME_BANCO = "rpgvida.sqlite";
+    private static final int VERSAO_BANCO = 1;
+
+    public PerfilDB(Context context) {
+        super(context, NOME_BANCO, null, VERSAO_BANCO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table if not exists perfil (" +
+                "_id integer PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
+                "nome text,  " +
+                "user text,  " +
+                "senha text, " +
+                "xp integer  " +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean savePerfil(Perfil perfil){
+        long id = perfil.getId();
+        SQLiteDatabase db = getWritableDatabase();
+        try {
+            ContentValues values = new ContentValues();
+            values.put("nome", perfil.getNome());
+            values.put("desc", perfil.getNome());
+            values.put("dificuldade", perfil.getNome());
+
+            if (id != 0) {
+                String _id = String.valueOf(perfil.getId());
+                String[] whereArgs = new String[]{_id};
+                int count = db.update("perfil", values, "_id=?", whereArgs);
+                return true;
+            } else {
+                values.put("feito", 0);
+                id = db.insert("missao", "", values);
+                return true;
+            }
+        } catch (Exception e){
+            return false;
+        }
+        finally {
+            db.close();
+        }
+    }
+    /****
+     * Validar função
+     ****/
+    public Perfil login(Perfil perfil){
+        Perfil perfil2 =  new Perfil();
+        SQLiteDatabase db = getWritableDatabase();
+        String where = " user = ? and senha = ?";
+        String[] whereArgs = new String[] {
+                perfil.getUser(),
+                perfil.getSenha()
+        };
+        try {
+            Cursor c = db.query("perfil", null,where, whereArgs , null, null, null, null);
+            if (c.moveToFirst()) {
+                perfil2.setId(c.getLong(c.getColumnIndex("_id")));
+                perfil2.setNome(c.getString(c.getColumnIndex("nome")));
+                perfil2.setUser(c.getString(c.getColumnIndex("user")));
+                perfil2.setXp(c.getInt(c.getColumnIndex("xp")));
+                perfil2.setLogged(true);
+            }
+        }finally {
+            db.close();
+        }
+
+        return perfil2;
+    }
+
+    public int delete(Perfil perfil) {
+        SQLiteDatabase db = getWritableDatabase();
+        try {
+            int count = db.delete("perfil", "_id=?", new String[]{String.valueOf(perfil.getId())});
+            Log.i(TAG, "Deletou [" + count + "] registro.");
+            return count;
+        } finally {
+            db.close();
+        }
+    }
+
+    public List<Perfil> findAll() {
+        SQLiteDatabase db = getWritableDatabase();
+        try {
+            Cursor c = db.query("perfil", null, null, null, null, null, null, null);
+            return toList(c);
+        } finally {
+            db.close();
+        }
+    }
+    // Lê o cursor e cria a lista de carros
+    private List<Perfil> toList(Cursor c) {
+        List<Perfil> perfils = new ArrayList<Perfil>();
+        if (c.moveToFirst()) {
+            do {
+                Perfil perfil = new Perfil();
+                perfils.add(perfil);
+                perfil.setId(c.getLong(c.getColumnIndex("_id")));
+                perfil.setNome(c.getString(c.getColumnIndex("nome")));
+                perfil.setUser(c.getString(c.getColumnIndex("user")));
+                perfil.setXp(c.getInt(c.getColumnIndex("xp")));
+                perfil.toString();
+            } while (c.moveToNext());
+        }
+        return perfils;
+    }
+
+    public Perfil findByID(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        Perfil perfil =  new Perfil();
+        try {
+            Cursor c = db.query("perfil", null,"_id=?", new String[]{id+""} , null, null, null, null);
+            if (c.moveToFirst()) {
+                perfil.setId(c.getLong(c.getColumnIndex("_id")));
+                perfil.setNome(c.getString(c.getColumnIndex("nome")));
+                perfil.setUser(c.getString(c.getColumnIndex("user")));
+                perfil.setXp(c.getInt(c.getColumnIndex("xp")));
+            }
+            return perfil;
+        }finally {
+            db.close();
+        }
+    }
+
+}
